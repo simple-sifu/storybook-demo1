bd0ff127205f16d1b4da097bf8e09e269384a284
Tommy Han <tommy.han@ownbackup.com>
2023-08-25 18:27:11 -0400

    add another color

--- src/stories/Light.stories.tsx
+++ src/stories/Light.stories.tsx
@@ -11,8 +11,28 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Base: Story = {
+export const Base: Story = {}
+
+export const Yellow: Story = {
     args: {
+        variant: 'red',
+    }
+}
+
+export const Red: Story = {
+    args: {
+        variant: 'red',
         
     }
 }
+
+export const Another: Story = {
+    args: {
+        variant: 'red'
+    },
+    render: (args) => <Light {...args} />,
+};
+
+export const AnotherExample: Story = {
+    render: () => <Light variant='blue' />,
+};
--- src/stories/Light.tsx
+++ src/stories/Light.tsx
@@ -1,7 +1,20 @@
-type Props = {};
+type Props = {
+    variant?: 'green' | 'yellow' | 'red' | 'blue'
+};
 
-const Light = (props: Props) => {
-    return <div>Light</div>;
+const Light = ({variant = 'green'}: Props) => {
+    return (
+        <div 
+            style={{
+                background: variant, 
+                borderRadius: '50%', 
+                width: 50, 
+                height: 50
+            }}
+        >
+            Light
+        </div>
+    );
 };
 
 export default Light;
